ea4a1880a3f6345952383f2d78e13b26c8d389b7
Zachary McCaw <zmccaw@Zacharys-MBP.attlocal.net>
2022-01-24 21:44:01 -0500

    Display issue

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -19,7 +19,7 @@ test_that("Reused subject IDs raises error.", {
 
 test_that("Test AUC comparison.", {
   
-  tau <- 5
+  tau <- 4
   
   GetAUCDiff <- function(data){
     out <-suppressWarnings(
@@ -71,11 +71,11 @@ test_that("Test AUC comparison.", {
   
   data0 <- data.frame(
     time = c(1, 2, 3, 4),
-    status = c(2, 2, 0, 1),
+    status = c(2, 2, 1, 0),
     idx = c(5, 6, 7, 8)
   )
   data0$arm <- 0
-  auc0 <- 0.5 * (tau - 4)
+  auc0 <- 0.25 * (tau - 3)
   
   data <- rbind(data1, data0)
   
